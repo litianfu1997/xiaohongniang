1ed37cf0480586cb8ac27845d6ec239975555e47
litianfu <1035869369@qq.com>
2019-09-08 18:02:37 +0800

    优化界面

--- pages/index/add/add.js
+++ pages/index/add/add.js
@@ -5,14 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    files: [],
+    loveText:null,
+    textLen:0,
   },
+  loveText(e){
+    console.log(e.detail)
+    this.setData({
+      loveText: e.detail.value,
+    })
+    var len = this.data.loveText.length
+    console.log(len)
+  },
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+      }
+    })
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+  selectFile(files) {
+    console.log('files', files)
+    // 返回false可以阻止某次文件上传
+  },
+  uplaodFile(files) {
+    console.log('upload files', files)
+    // 文件上传的函数，返回一个promise
+    return new Promise((resolve, reject) => {
+      //上传图片
+      wx.uploadFile({
+        url: 'http://localhost:8080/xhn/uploadfile',
+        filePath: files.tempFilePaths,
+        name: 'file',
+        success(res) {
+          resolve({ urls: "" })
+        }, complete() {
+          resolve({ urls: "" })
+        }
+      })
+      setTimeout(() => {
+        reject('some error')
+      }, 1000)
+    })
+  },
+  uploadError(e) {
+    console.log('upload error', e.detail)
+  },
+  uploadSuccess(e) {
+    console.log('upload success', e.detail)
+  },
+ 
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      selectFile: this.selectFile.bind(this),
+      uplaodFile: this.uplaodFile.bind(this)
+    })
   },
 
   /**
